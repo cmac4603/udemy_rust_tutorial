14274451910ebade0f89c5e8fa3e83d5edd72631
Colin MacRae <colin.macrae@hearst.co.uk>
2017-04-22 18:57:26 +0100

    completed lecture 18

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+use std::mem;
 
 enum Color {
     Red,
@@ -47,7 +48,48 @@ fn option() {
     if let Some(z) = result { println!("z = {}", z); };
 }
 
+fn arrays() {
+    let mut a:[i32;5] = [1,2,3,4,5];
+
+    println!("a has {} elements, first is {}", a.len(), a[0]);
+
+    a[0] = 321;
+    println!("a[0] = {}", a[0]);
+
+    println!("{:?}", a);
+
+    if a != [321,2,3,4,5] {
+        println!("does not match");
+    }
+    else {
+        println!("does match!");
+    }
+
+    let b = [1u16;10]; // b.len() == 10
+    for i in 0..b.len(){
+        println!("{}", b[i]);
+    }
+
+    println!("b took up {} bytes", mem::size_of_val(&b));
+
+    let mtx:[[f32;3];2] =
+    [
+        [1.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0]
+    ];
+    println!("{:?}", mtx);
+
+    for i in 0..mtx.len() {
+        for j in 0..mtx[i].len(){
+            if i == j {
+                println!("mtx[{}][{}] = {}", i, j, mtx[i][j]);
+            }
+        }
+    }
+}
+
 fn main () {
     //enums();
-    option();
+    //option();
+    arrays();
 }

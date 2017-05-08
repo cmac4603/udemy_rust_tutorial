515b7f6fcd0636eb95fc5bdde306351ff2317f8f
Colin MacRae <cmac4603@users.noreply.github.com>
2017-05-08 20:30:13 +0100

    completed lecture 28 - higher order functions

--- src/main.rs
+++ src/main.rs
@@ -103,9 +103,35 @@ fn closures() {
     println!("f = {}", f);
 }
 
+fn is_even(x:i32) -> bool {
+    x % 2 == 0
+}
+
+fn hof() {
+    // iterative programming
+    let limit = 500;
+    let mut sum = 0;
+    for i in 0.. {
+        let isq = i*i;
+
+        if isq > limit { break; }
+        else if is_even(isq) { sum += isq }
+    }
+    println!("loop sum = {}", sum);
+
+    // more functional programming using primitives
+    let sum2 =
+        (0..).map(|x| x*x)
+             .take_while(|&x| x <= limit)
+             .filter(|&x| is_even(x))
+             .fold(0, |sum,x| sum + x);
+    println!("hof sum = {}", sum2);
+}
+
 fn main() {
     // generics();
     // functions();
     // methods();
-    closures();
+    // closures();
+    hof();
 }

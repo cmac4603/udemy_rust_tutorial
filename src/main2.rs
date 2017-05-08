02476f54e0a1c9e19a9e7e53e8fb099a8f0a4c31
Colin MacRae <cmac4603@users.noreply.github.com>
2017-05-08 07:29:50 +0100

    new main.rs for clean workspace

--- src/main2.rs
+++ src/main2.rs
@@ -1,112 +1,246 @@
-#![allow(unused_variables)]
 #![allow(dead_code)]
-mod sh;
-mod if_exp;
-mod while_loops;
-mod for_loops;
-mod matches;
-mod structures;
+#![allow(unused_variables)]
 use std::mem;
+mod pm;
 
-const MEANING_OF_LIFE:i8 = 42; // no fixed address
-
-static mut Z_TWO:i32 = 132;
-
-fn fundamental_data_types() {
-    
-    // i8 u8 i16 u16 i32 u32
-    let a:u8 = 123; // 8bits 0..255
-    let  mut b:i8 = 0; // 8bits -255..255 and mutable
-    println!("a = {}, b = {}", a, b);
-
-    b = 42;
-    println!("a = {}, b = {}", a, b);
-
-    let mut c = 123456789;
-    println!("c = {}, size = {} bytes", c, mem::size_of_val((&c)));
-    c = -1;
-    println!("c = {} after mod", c);
-
-    let z:isize = 123; // isize/usize
-    let size_of_z = mem::size_of_val(&z);
-    println!("z = {}, takes up {} bytes, {}-bit OS",
-      z,size_of_z, size_of_z*8);
-
-    let d:char = 'x';
-    println!("d = {}, size = {} bytes", d, mem::size_of_val((&d)));
-
-    let e:f32 = 2.5; // double-precision value, 8bytes or 64 bits, f64
-    println!("e = {}, size = {} bytes", e, mem::size_of_val((&e)));
-
-    // true or false
-    let g = false;
-    println!("g = {}, size = {} bytes", g, mem::size_of_val((&g)));
-
-    let f = 4>0; //true
-    println!("f = {}, size = {} bytes", f, mem::size_of_val((&f)));
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8,u8,u8), // tuple
+    CmykColor{cyan:u8, magenta:u8, yellow:u8, black:u8}, // struct
 }
 
-fn operators() {
-    // arithmetic
-    let mut a = 2+3*4; //+-*/
-    println!("a = {}", a);
-    a = a+1;
-    a -= 2; // -= += /= %=
-    println!("remainder of {} / {} = {}", a, 3, (a%3));
-    let a_cubed = i32::pow(a, 3);
-    println!("{} cubed is {}", a, a_cubed);
+fn enums() {
+    // let c:Color = Color::RgbColor(0,0,10);
+    // let c:Color = Color::CmykColor{cyan: 0, magenta: 128, yellow: 0, black: 0};
+    let c:Color = Color::CmykColor{cyan: 0, magenta: 128, yellow: 0, black: 255};
 
-    let b = 2.5;
-    // integral power
-    let b_cubed = f64::powi(b, 3);
-    println!("\n{} cubed is {}", b, b_cubed);
-    // float power
-    let b_to_pi = f64::powf(b, std::f64::consts::PI);
-    println!("{} to the power of pi = {}", b, b_to_pi);
-
-    //bitwise
-    let c = 1 | 2; // | OR & AND ^ XOR ! NOR
-                   // 01 OR 10 = 11 == 3_10
-    println!("1|2 = {}", c);
-
-    let two_to_10 = 1 << 10; // >>
-    println!("2^10 = {}", two_to_10);
-
-    // logical
-    let pi_less_4 = std::f64::consts::PI < 4.0; //true
-    // < > <= >= ==
-    let x = 5;
-    let x_is_5 = x == 5; // true
-    println!("is pi less than 4 -- {}", pi_less_4);
-    println!("x is equal to 5 -- {}", x_is_5);
-}
-
-fn scope_and_shadowing() {
-    let a = 123;
-    let a = 777;
-    {
-        let b = 456;
-        println!("inside, b = {}", b);
-        let a = 888;
-        println!("inside, a = {}", a);
+    match c {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Blue => println!("blue"),
+        Color::RgbColor(0,0,0) 
+        | Color::CmykColor{cyan:_,magenta:_,yellow:_,black:255}=> println!("black"),
+        Color::RgbColor(r,g,b) => println!("rgh({},{},{})", r, g, b),
+        _ => ()
     }
-    println!("outside a = {}", a);
 }
 
-fn main() {
-    // fundamental_data_types();
-    // operators();
-    // scope_and_shadowing();
-    //unsafe
-    //{
-    //    Z_TWO = 125;
-    //    println!("{}", Z_TWO);
-    //}
-    //println!("{}", Z_TWO);
-    //sh::stack_and_heap();
-    //if_exp::if_statement();
-    //while_loops::while_and_loop();
-    //for_loops::for_loop();
-    //matches::match_statement();
-    structures::structures();
+fn option() {
+    // Option<T>
+
+    let x = 3.0;
+    let y = 0.0;
+
+    // option can be Some(z) or None
+
+    let result:Option<f64> =
+        if y != 0.0 { Some(x/y) } else {None};
+
+    println!("{:?}", result);
+
+    match result {
+        Some(z) => println!("{}/{} = {}", x, y , z),
+        None => println!("Cannot divide {} by {}", x, y),
+    }
+
+    // if let / while let
+    if let Some(z) = result { println!("z = {}", z); };
+}
+
+fn arrays() {
+    let mut a:[i32;5] = [1,2,3,4,5];
+
+    println!("a has {} elements, first is {}", a.len(), a[0]);
+
+    a[0] = 321;
+    println!("a[0] = {}", a[0]);
+
+    println!("{:?}", a);
+
+    if a != [321,2,3,4,5] {
+        println!("does not match");
+    }
+    else {
+        println!("does match!");
+    }
+
+    let b = [1u16;10]; // b.len() == 10
+    for i in 0..b.len(){
+        println!("{}", b[i]);
+    }
+
+    println!("b took up {} bytes", mem::size_of_val(&b));
+
+    let mtx:[[f32;3];2] =
+    [
+        [1.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0]
+    ];
+    println!("{:?}", mtx);
+
+    for i in 0..mtx.len() {
+        for j in 0..mtx[i].len(){
+            if i == j {
+                println!("mtx[{}][{}] = {}", i, j, mtx[i][j]);
+            }
+        }
+    }
+}
+
+fn vectors() {
+    let mut a = Vec::new();
+    a.push(1);
+    a.push(2);
+    a.push(3);
+
+    println!("a = {:?}", a);
+
+    a.push(44);
+    println!("a = {:?}", a);
+    
+    let idx:usize = 0;
+    a[idx] = 321;
+    println!("a[0] = {:?}", a[idx]);
+
+    // .get() on vector returns Option
+    // match a.get(6) {
+    match a.get(3) {
+        Some(x) => println!("a[4] = {}", x),
+        None => println!("error no such element"),
+    }
+
+    for x in &a {
+        println!("{}", x);
+    }
+
+    a.push(77);
+    println!("{:?}", a);
+
+    let last_elem = a.pop(); // Option
+    println!("{:?}", last_elem); // Some(x) if vector not empty
+    println!("last_elem is {:?}, left with vector a = {:?}", last_elem, a); // Some(x) if vector not empty
+
+    // let last_elem = a.pop(); // Option
+    // let last_elem = a.pop(); // Option
+    // let last_elem = a.pop(); // Option
+    // let last_elem = a.pop(); // Option
+    // let last_elem = a.pop(); // Option
+
+    // match last_elem {
+    //     Some(x) => println!("{}", x),
+    //     None => println!("vector a is empty"),
+    // }
+
+    // how to iterate over Option returns,
+    // this will fail when a.pop() returns None
+    while let Some(x) = a.pop() {
+        println!("In Some(x), x = {}", x);
+    }
+}
+
+fn use_slice(slice: &mut[i32]) {
+    println!("first elem in slice = {}, len of slice = {}",
+             slice[0], slice.len());
+    slice[0] = 4321;
+}
+
+fn slices() {
+    let mut data = [1,2,3,4,5];
+
+    use_slice(&mut data[1..4]);
+    // use_slice(&mut data);
+    println!("returned data array is: {:?}", data)
+}
+
+fn strings() {
+    // utf-8
+    // &'static sets the lifetime of s to live for the entire life of the program
+    let s:&'static str = "hello there!"; // &str = static string
+    println!("{}", s);
+
+    // &'static str cannot be manipulated by this
+    // let s = 'abc';
+    // below doesn't work because you would be trying to index the bytes
+    // let h = s[0];
+
+    for c in s.chars().rev() {
+        println!("{}", c);
+    }
+
+    if let Some(first_char) = s.chars().nth(0) {
+        println!("first character of s is {}", first_char);
+    }
+
+    // String - a heap allocated construct
+    let mut letters = String::new();
+    let mut a = 'a' as u8;
+    println!("a as u8 = {}", a);
+    while a <= ('z' as u8) {
+        letters.push(a as char);
+        letters.push_str(",");
+        a += 1;
+    }
+
+    println!("{}", letters);
+
+    // &str < > String
+    let u:&str = &letters;
+    println!("{}", u);
+
+    let z = u.to_owned() + u;
+    println!("{}", z);
+
+    // let mut abc = String::from("hello world");
+    let mut abc = "hello world".to_string();
+    abc.remove(0);
+    abc.push_str("!!!");
+    println!("{}", abc.replace("ello", "goodbye"));
+}
+
+fn sum_and_product(x: i32, y:i32) -> (i32, i32)  {
+    // arrays all have to be of the same type
+    // tuples can be a mismatch of types
+    (x+y, x*y)
+}
+
+fn tuples() {
+    let x = 3;
+    let y = 4;
+    let sp = sum_and_product(x,y);
+    println!("sp = {:?}", sp);
+    println!("{0} + {1} = {2}, {0} * {1} = {3}", x, y, sp.0, sp.1);
+
+    // destructuring
+    let (a, b) = sp;
+    println!("a = {}, b = {}", a, b);
+
+    let sp2 = sum_and_product(4, 7);
+    let combined = (sp, sp2);
+    println!("{:?}", combined);
+    println!("last elem = {}", (combined.1).1);
+
+    // destructuring again
+    let ((c,d),(e,f)) = combined;
+    println!("{} {} {} {}", c, d, e, f);
+
+    // mismatched tuple
+    let foo = (true, 42.0, -1i8);
+    println!("mismatched foo = {:?}", foo);
+
+    // single element tuple
+    let meaning = (42,);
+    println!("{:?}", meaning);
+}
+
+fn main () {
+    // enums();
+    // option();
+    // arrays();
+    // vectors();
+    // slices();
+    // strings();
+    // tuples();
+    pm::pattern_matching();
 }

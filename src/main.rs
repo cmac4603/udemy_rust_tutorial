795a2260686f9fcfc19b0392ad9fe0f3b4d5390c
Colin MacRae <cmac4603@users.noreply.github.com>
2017-05-08 08:08:33 +0100

    completed lecture 26 - methods

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 // Option<T> is for defining types
-// below is how one can define multiple types in a struct
+// below is how one or more types in a struct
 // struct Point<T, V> {
 //     x:T,
 //     y:V,
 // }
 
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point {
+    x: f64,
+    y: f64,
 }
 
-struct Line<T> {
-    start: Point<T>,
-    end: Point<T>,
+struct Line {
+    start: Point,
+    end: Point,
 }
 
 fn generics() {
     // let a:Point<u16, i32> = Point { x: 0.3, y: 4 };
     // let b:Point = Point { x: 1.2, y: 3.4};
-    let a:Point<f64> = Point { x: 0.0, y: 4f64 };
+    let a:Point = Point { x: 0.0, y: 4f64 };
     let b = Point { x: 1.2, y: 3.4};
 
     let myline = Line { start: a, end: b};
@@ -51,7 +51,24 @@ fn functions() {
     println!("{} * {} = {}", a, b, p);
 }
 
+impl Line {
+    fn len(&self) -> f64 {
+        let dx = self.start.x - self.end.x;
+        let dy = self.start.y - self.end.y;
+        (dx * dx + dy * dy).sqrt()
+    }
+}
+
+fn methods() {
+    let p = Point { x: 3.0, y: 4.0 };
+    let p2 = Point { x: 5.0, y: 10.0 };
+    let myline = Line { start: p, end: p2};
+
+    println!("length = {}", myline.len());
+}
+
 fn main() {
     // generics();
-    functions();
+    // functions();
+    methods();
 }

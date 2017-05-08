22e3061f623c68e80bf6a41535c792a7573fbde8
Colin MacRae <cmac4603@users.noreply.github.com>
2017-05-08 07:56:44 +0100

    completed lecture 25 - functions

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,32 @@ fn generics() {
     let myline = Line { start: a, end: b};
 }
 
+fn print_value(x: i32) {
+    println!("value = {}", x);
+}
+
+fn increase(x: &mut i32){
+    *x += 1;
+}
+
+fn product(x: i32, y:i32) -> i32 {
+    x * y
+}
+
+fn functions() {
+    print_value(33);
+
+    let mut z = 1;
+    increase(&mut z);
+    println!("z = {}", z);
+
+    let a = 3;
+    let b = 5;
+    let p = product(3, 5);
+    println!("{} * {} = {}", a, b, p);
+}
+
 fn main() {
-    generics();
+    // generics();
+    functions();
 }
